beaec29a79c2eda1ecf476a485d5574ecfa568f9
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-28 18:41:46 +0200

    Adding custom Header Component

---
+++ src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+    // template: '<h1>Title</h1>'
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+
+export class HeaderComponent {}
