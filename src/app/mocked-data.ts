6023501d8e82ddb155ec730ff2444a2d0dba4018
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-29 19:29:04 +0200

    Add ContentComponent

--- src/app/mocked-data.ts
+++ src/app/mocked-data.ts
@@ -23,5 +23,23 @@ export const TUTS_DATA = [
         id: 5,
         title: 'Concepts',
         url: '/reference/concepts'
-    }
+    },
 ];
+
+export const HELP_ARTICLES = [
+    {
+        id: 1.1,
+        title: 'CSS Grid Layout – Holy Grail Angular 16 UI Tutorial',
+        url: 'https://www.positronx.io/css-grid-layout-holy-grail-angular-ui-tutorial/'
+    },
+    {
+        id: 1.2,
+        title: 'HTML attributes vs DOM properties',
+        url: 'https://jakearchibald.com/2024/attributes-vs-properties/'
+    },
+    {
+        id: 1.3,
+        title: 'Harmonizing Your Angular Project: The Importance of Normalize.css',
+        url: 'https://medium.com/@shivam29feb/harmonizing-your-angular-project-the-importance-of-normalize-css-5f3e32b82b0c'
+    }
+]
