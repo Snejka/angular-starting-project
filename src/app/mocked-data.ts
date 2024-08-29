2bf735f6d97543eb228bd3b188182999e423abdd
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-29 18:40:46 +0200

    Creatin a Dummy data for loading content dynamicaly

---
+++ src/app/mocked-data.ts
@@ -0,0 +1,27 @@
+export const TUTS_DATA = [
+    {
+        id: 1,
+        title: 'Learn Angular in your browser',
+        url: 'https://angular.dev/tutorials/learn-angular'
+    },
+    {
+        id: 2,
+        title: 'Build your first Angular app locally',
+        url: 'https://angular.dev/tutorials/first-app'
+    },
+    {
+        id: 3,
+        title: 'Just need the Docs?',
+        url: 'https://angular.dev/overview'
+    },
+    {
+        id: 4,
+        title: 'API Reference',
+        url: 'https://angular.dev/api'
+    },
+    {
+        id: 5,
+        title: 'Concepts',
+        url: 'https://angular.dev/reference/concepts'
+    }
+];
