f52fc25deb25f6f1ac9227f101c78afe70d4e35a
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-29 14:54:46 +0200

    Adding a sidebar section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 // TypeScript Decorator
 // Add extra data
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
